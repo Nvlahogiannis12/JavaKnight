32b7360cc9fa75b07f838ae408dbd3b670ee0136
Nvlahogiannis12 <526Nvlahogiannis@frhsd.com>
2025-04-19 20:04:49 +0000

    No idea why that just broke then fixed itself???

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 let playerMenuOptions = ["Start", "Options",];
 let index = 0;
 let firstOptions = true
-let secondOptions = false
+
+window.onload = function(){
 isMenu()
 updatePlayerActionOption()
-
+}
 //Arrow Keys for selections if selections go over then they loop
 function playerKeyDownsMenuOne(event){
     if(firstOptions === false) return;
@@ -58,14 +59,63 @@ function playerKeyDownsMenuOne(event){
   //Start Menu Appears to Pick Dungeon
   function selectDungeon(){
     firstOptions = false
-    secondOptions = true
     document.getElementById('startMenuBG').classList.add('d-none')
     document.getElementById('gameSelectDungeon').classList.remove('d-none')
     isMenu()
   }
 
+
+  //START OF CODE FOR ACTUAL GAME
+
+  
+
+  //GAME VARIABLES
+  let previousPlayerTurnMoves = [];
+  let previousOpponentTurnMoves = [];
+
+  //when button is clicked this will get the location (variable) and save it across HTML Pages
   function startGame(location, destinationPage){
     localStorage.setItem("level", location)
     window.location.href = destinationPage
   }
 
+  let level = localStorage.getItem("level");
+  if(level === 'forest' || level === 'seas' || level === 'volcano'){
+    genLoadingType()
+  }
+//random number
+  function getLoadingNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+//Random Number Creates A Message For Load Screen (Maybe tips for player in Future)
+function genLoadingType() {
+  let loadingNumber = getLoadingNumber(0, 3);
+  if (loadingNumber === 0){
+    document.getElementById('loadingType').innerText =`Traveling...`
+  } else if (loadingNumber === 1){
+    document.getElementById('loadingType').innerText =`Discovering...`
+  } else if (loadingNumber === 2){
+    document.getElementById('loadingType').innerText =`Journeying...` 
+  } else if (loadingNumber === 3){
+    document.getElementById('loadingType').innerText =`Preparing...` 
+  }
+  //after 4 seconds (4000 milliseconds) it calls this function to make it vanish
+  setTimeout(loadScreenVanish, 4000,)
+}
+//what to do after time delay in genLoadingType
+function loadScreenVanish(){
+  if(level === 'forest'){
+    document.body.style.backgroundImage = "url('imgs/ForestBG.jpg')"
+    document.getElementById('loadingType').classList.add('d-none')
+    document.getElementById('playArea').classList.remove('d-none')
+  } else if(level === 'seas'){
+    document.body.style.backgroundImage = "url('imgs/Shores.webp')"
+    document.getElementById('loadingType').classList.add('d-none')
+    document.getElementById('playArea').classList.remove('d-none')
+  } else if(level === 'volcano'){
+    document.body.style.backgroundImage = "url('imgs/Volcanic-Wastelands.png')"
+    document.getElementById('loadingType').classList.add('d-none')
+    document.getElementById('playArea').classList.remove('d-none')
+  }
+}
